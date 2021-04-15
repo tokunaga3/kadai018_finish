3415dcd2aaa105f781bc1e3aaa6338f055fe0882
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-04-15 06:56:32 +0000

    createアクションからのjsファイルの読み込み成功

--- app/controllers/event_comments_controller.rb
+++ app/controllers/event_comments_controller.rb
@@ -3,11 +3,12 @@ before_action :event_comment_params
   def create
     @comment = EventComment.new(event_comment_params)
     @comment.event_id = params[:format].to_i
-    if @comment.save
-      redirect_to events_path,notice: "イベントを追加しました！"
-    else
-      render :new
-    end
+      if @comment.save
+        # binding.pry
+      else
+        # render :new
+        redirect_to event_path,notice: "コメントできませんでした。！"
+      end
   end
 
   private
