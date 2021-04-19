5de30241018e872eb7e25bcb52d6caab29962b34
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2021-04-19 01:35:47 +0000

    コメントをユーザー毎で位置を分けた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def users_comments_classchage(comment_user)
     if current_user.id == comment_user.id
-      "current_user_class"
+      "current_user_class offset-6 col-6"
     else
-      "other_user_class"
+      "other_user_class col-6 ofset-6"
     end
   end
 
